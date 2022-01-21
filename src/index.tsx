1d9247dd9b9b4ca2cc84d926f72311176b7413bd
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-01-21 18:50:26 -0300

    ✨ feat: Card hover animation

--- src/index.tsx
+++ src/index.tsx
@@ -16,6 +16,12 @@ ReactDOM.render(
       estudio='OLM' duracao='24 min' ultimoEpisodio='12' 
       episodioNovo 
     />
+    <CardAnime 
+      imagem='https://cdn.myanimelist.net/images/characters/5/354941.jpg?_gl=1*rlsxe3*_ga*MTE0NDQ3NDg5LjE2MzQ4MzYyOTE.*_ga_26FEP9527K*MTY0Mjc3OTU1OS4xMC4xLjE2NDI3Nzk4OTIuMjk.' 
+      nome= "Komi-san wa, Comyushou desu." genres="Comedy, Slice of Life." 
+      estudio='OLM' duracao='24 min' ultimoEpisodio='12' 
+      episodioNovo 
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
